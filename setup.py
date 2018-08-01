1c1dd7bf299fd904945429d770e25c5f59199a9d
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-08-01 15:06:10 -0400

    Add version to class and bump to 2.0.12

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfdicom_tagExtract',
-      version          =   '2.0.10',
+      version          =   '2.0.12',
       description      =   'Extract DICOM header info -- part of the pf* family.',
       long_description =   readme(),
       author           =   'FNNDSC',
